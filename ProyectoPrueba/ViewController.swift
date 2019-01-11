ce638a641a734024f5504e987d2f57761b9a4266
El tiguere <julihoyosm@gmail.com>
2019-01-11 20:34:18 +0100

    Primer commit de prueba

--- ProyectoPrueba/ViewController.swift
+++ ProyectoPrueba/ViewController.swift
@@ -11,7 +11,6 @@ class ViewController0: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view.
     }
     
